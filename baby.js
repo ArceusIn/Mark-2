97a776ce6a6754f460bcc94d47bc105bcf226778
Saurabh kr <saurabh65000@gmail.com>
2022-12-29 21:50:52 +0530

    .

---
+++ baby.js
@@ -0,0 +1,6 @@
+// background: linear-gradient(338deg, hsl(164deg 100% 59%), hsl(49deg 100% 62%));
+
+// --font-family-body: "Heebo",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Oxygen,Ubuntu,Cantarell,"Open Sans",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+
+// --font-family-mono: ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro","Fira Mono", "Droid Sans Mono", "Courier New", monospace;
+
